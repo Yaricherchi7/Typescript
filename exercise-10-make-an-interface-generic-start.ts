33e19526f20fd59cea576fc1c48cb96ccb23d131
Yari Cherchi <yari.cherchi7@gmail.com>
2023-02-16 11:46:02 +0100

    es svolti

--- exercise-10-make-an-interface-generic-start.ts
+++ exercise-10-make-an-interface-generic-start.ts
@@ -6,19 +6,19 @@
 // This should fix the type errors on Lines 14 and 21.
 // Hint: The type for the `languages` property should use a type variable.
 
-interface Country {
+interface  languagesType <type> {
     name: string;
-    languages: unknown;
+    languages: type;
 }
 
-const languagesObj1: Country<string> = {
+const languagesObj1: languagesType<string> = {
     name: "New Zealand",
     languages: "English, Māori"
 };
 
 console.log(languagesObj1.languages);
 
-const languagesObj2: Country<string[]> = {
+const languagesObj2: languagesType<string[]> = {
     name: "Spain",
     languages: ["Spanish", "Catalan", "Galician", "Basque", "Valencian"],
 };
